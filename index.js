f1c40a3bf4579bd0efb0aa86e758ab7d42a4f5c4
Tejas Gupta <131871888+multiverseweb@users.noreply.github.com>
2024-01-16 05:05:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var sq=document.getElementById("square");
+var st=document.getElementById("statue");
+var nav=document.getElementById("navbar");
+var left=document.getElementById("left");
+function changeCss() {
+    this.scrollY > 10 ? statue.style.opacity = 0 : statue.style.opacity = 1;
+    this.scrollY > 10 ? square.style.opacity = 0 : square.style.opacity = 1;
+    this.scrollY > 10 ? nav.style.backgroundColor = "rgba(177, 177, 177, 0.14)" : nav.style.backgroundColor = "#00000000";
+    this.scrollY > 10 ? nav.style.backdropFilter = "blur(5px)" : nav.style.backdropFilter="none";
+}
+
+window.addEventListener("scroll", changeCss, false);
+
+
+const targetCount = 1024;
+const counterElement = document.getElementById('collections');
+function updateCounter(count) {
+    counterElement.textContent = count;
+}
+function startCounter() {
+    let count = 1000;
+    const intervalId = setInterval(function () {
+        if (count < targetCount) {
+            count++;
+            updateCounter(count);
+        } else {
+            clearInterval(intervalId);
+        }
+    }, 100);
+}
+window.onload = startCounter;
