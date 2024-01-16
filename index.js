2e5f441c87257123d562bb89efeadf4d97a391cc
Tejas Gupta <131871888+multiverseweb@users.noreply.github.com>
2024-01-16 09:13:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ var sq=document.getElementById("square");
 var st=document.getElementById("statue");
 var nav=document.getElementById("navbar");
 var left=document.getElementById("left");
+var fixed=document.getElementById("fixed");
 function changeCss() {
     this.scrollY > 10 ? statue.style.opacity = 0 : statue.style.opacity = 1;
     this.scrollY > 10 ? square.style.opacity = 0 : square.style.opacity = 1;
     this.scrollY > 10 ? nav.style.backgroundColor = "rgba(177, 177, 177, 0.14)" : nav.style.backgroundColor = "#00000000";
     this.scrollY > 10 ? nav.style.backdropFilter = "blur(5px)" : nav.style.backdropFilter="none";
+    this.scrollY > 140 ? fixed.style.opacity="0" : fixed.style.opacity=1;
+    this.scrollY > 140 ? document.getElementById("c1").style.height="46vh" : document.getElementById("c1").style.height="0vh";
+    this.scrollY > 300 ? document.getElementById("c2").style.width="27vw" : document.getElementById("c2").style.width="0vw";
+    this.scrollY > 330 ? document.getElementById("info").style.opacity="1" : document.getElementById("info").style.opacity="0";
+    this.scrollY > 330 ? document.getElementById("info").style.marginLeft="5vw" : document.getElementById("info").style.marginLeft="3vw";
 }
 
 window.addEventListener("scroll", changeCss, false);
